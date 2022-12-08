c76a0209509a74dc7f73785b6c672b2d1930c801
Tenno641 <120097440+Tenno641@users.noreply.github.com>
2022-12-09 01:13:39 +0200

    deciphering
    deciphering by multiplying the encrypted text matrix by the new key

--- Hill Cipher.py
+++ Hill Cipher.py
@@ -55,3 +55,20 @@ def inv_of_2x2_matrix(invKey):
     deciphering(lettersMatrix, newKey)
 
 
+def deciphering(arr, deKey):
+    ciphertext2 = []
+
+    if len(arr) % 2 == 1:
+        arr.append(0)
+
+    times = len(arr)
+
+    i = 0
+    for x in range(0, int(times / 2 )):
+        ciphertext2.append((arr[i] * deKey[0] + arr[i+1] * deKey[1]).__mod__(26))
+        ciphertext2.append((arr[i] * deKey[2] + arr[i+1] * deKey[3]).__mod__(26))
+        i += 2
+
+    matrix_to_text(ciphertext2)
+
+
